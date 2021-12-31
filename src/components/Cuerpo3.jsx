de6a763a7729294879f0ff10d3a967125717846d
icharrd <ricardosv46@gmail.com>
2021-12-30 22:31:37 -0500

    first commit

--- src/components/Cuerpo3.jsx
+++ src/components/Cuerpo3.jsx
@@ -7,7 +7,7 @@ const Cuerpo3 = () => {
         <div className='flex flex-row  justify-center  '>
           <h1 className='font-bold text-3xl lg:text-5xl '>¿Por qué nosotros?</h1>
         </div>
-        <div className='flex flex-col gap-6  items-center lg:items-stretch pt-10 p-10 lg:flex-row lg:justify-center lg:gap-40 bg-verde'>
+        <div className='flex flex-col gap-6  items-center lg:items-stretch pt-10 p-10 lg:flex-row lg:justify-center lg:gap-40'>
           <p className='text-9xl text-blue-900 flex flex-col font-bold'>+5<span className='text-2xl text-black font-medium text-center mt-0 lg:mt-5'>Más de 5 años de <br />experiencia</span></p>
           <p className='  text-9xl text-blue-900 flex flex-col font-bold'>50<span className='text-2xl text-black font-medium text-center mt-0 lg:mt-5'>profesores e <br />instructores.</span></p>
 
